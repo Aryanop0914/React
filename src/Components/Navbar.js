ffc40eeb7e97dca7047ee48539b20a8afcc11621
Aryan0914 <21cs038@charusat.edu.in>
2023-01-19 20:55:24 +0530

    navbar done effect left

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
       <right>
         <div className="menu">
           <ul>
-            <li><a href="">Home</a></li>
+            <li className="active"><a href="">Home</a></li>
             <li><a href="">For Owners</a></li>
             <li><a href="">About Us</a></li>
             <li><a href="">Contact</a></li>

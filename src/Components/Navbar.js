086342613d45b7001d586c5ee23611af034f5b24
Aryan0914 <21cs038@charusat.edu.in>
2023-01-19 20:38:35 +0530

    j

---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+
+     <div className="logo">
+         <h3>Homestay<span>.</span></h3>
+     </div>
+          
+      <right>
+        <div className="menu">
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">For Owners</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </div>
+      </right>
+     
+     
+     </nav>
+  )
+}

6b3477237f0bec032ef4f9322bb54a22c62fdc4c
Dhruvn1403 <21cs040@charusat.edu.in>
2023-01-30 10:16:47 +0530

    info

---
+++ src/Components/Info.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function info() {
+  return (
+    <div className="info">
+    <div className="container">
+        <div className="row">
+            <div className='col'>
+               <h1>This is info</h1> 
+               <p>abc</p>
+           </div>
+           <div className="col" >
+               <h1>This is info</h1> 
+               <p>abc</p>
+           </div>
+           <div className="col" >
+               <h1>This is info</h1> 
+               <p>abc</p>
+           </div>
+        </div>
+     
+    </div>
+    </div>
+  )
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import Navbar from '../Components/Navbar'
 import Header1 from '../Components/Header1'
 import About from '../Components/About'
 import Contact from '../Components/Contact'
+import Info from '../Components/Info'
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
        <Navbar/>
        <Header1/>
        <About/>
+       <Info/>
        <Contact/>
     </>
   )

a95b2709bbfd2b8859cc7b1d84d2380ec67863a3
Dhruv <21cs040@charusat.edu.in>
2023-01-30 23:24:22 +0530

    IInfo

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -6,16 +6,28 @@ export default function info() {
     <div className="container">
         <div className="row">
             <div className='col'>
-               <h1>This is info</h1> 
-               <p>abc</p>
+               <div className="service-item">
+                        <img className="img" src="./images/IInfo/True_Values.png" alt=""/>
+                        <h4><b>True Value</b></h4>
+                        <p>We don't just offer great prices and good long term accommodation deals,
+                             we provide true value for money by giving you a priceless authentic experience.</p>
+                    </div>
            </div>
            <div className="col" >
-               <h1>This is info</h1> 
-               <p>abc</p>
+               <div className="service-item">
+                        <img className="img" src="./images/IInfo/Real_Homes.png" alt=""/>
+                        <h4><b>Real Homes</b></h4>
+                        <p>We will always offer rooms in real homes with local hosts.
+                             Our accommodation is different because you stay with locals and become part of the local community.</p>
+                    </div>
            </div>
            <div className="col" >
-               <h1>This is info</h1> 
-               <p>abc</p>
+               <div className="service-item">
+                        <img className="img" src="./images/IInfo/Sustainable_tourism.png" alt=""/>
+                        <h4><b>Sustainable Tourism</b></h4>
+                        <p>We promote sustainable local tourism by ensuring you're spending stays
+                             within local communities, minimising your environmental impact.</p>
+                    </div>
            </div>
         </div>
      

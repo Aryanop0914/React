f734359c4b3fe9ee6e78533078cc937fc5ba64a1
Dhruv <21cs040@charusat.edu.in>
2023-01-31 15:29:33 +0530

    change

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -6,24 +6,7 @@ export default function info() {
     <div className="container">
         <div className="row">
             <div className='col'>
-<<<<<<< HEAD
-                <img src="./images/Favicons/True_Values.png" alt=""/>
-                <h4>True Value</h4>
-                <p>We don't just offer great prices and good long term accommodation deals,
-                  we provide true value for money by giving you a priceless authentic experience.</p>
-           </div>
-           <div className="col" >
-                <img src="./images/Favicons/Real_Homes.png" alt=""/>
-                <h4>Real Homes</h4>
-                <p>We will always offer rooms in real homes with local hosts.
-                    Our accommodation is different because you stay with locals and become part of the local community.</p>
-           </div>
-           <div className="col" >
-                <img src="./images/Favicons/Sustainable_tourism.png" alt="" />
-                <h4>Sustainable Tourism</h4>
-                <p>We promote sustainable local tourism by ensuring you're spending stays
-                    within local communities, minimising your environmental impact.</p>
-=======
+
                <div className="service-item">
                         <img className="img" src="./images/IInfo/True_Values.png" alt=""/>
                         <h4><b>True Value</b></h4>
@@ -45,8 +28,8 @@ export default function info() {
                         <h4><b>Sustainable Tourism</b></h4>
                         <p>We promote sustainable local tourism by ensuring you're spending stays
                              within local communities, minimising your environmental impact.</p>
-                    </div>
->>>>>>> 9155687730b623182f0ddbf2e3773cc3aaf14e15
+                    </div> 
+
            </div>
         </div>
      
